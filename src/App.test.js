8b48a2fe8b2dcee5d6dd01f27cc33e62706e67c4
Bradley Davis <Bradley-D@users.noreply.github.com>
2022-07-06 22:44:37 +1000

    Update test after initial strava request test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders strava activities heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const stravaText = screen.getByText(/Last Strava activities/i);
+  expect(stravaText).toBeInTheDocument();
 });
