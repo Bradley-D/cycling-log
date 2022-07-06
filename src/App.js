df6bda0eeecffc70cc2fbd6b63db2bb1fbcbe84f
Bradley Davis <Bradley-D@users.noreply.github.com>
2022-07-06 22:40:57 +1000

    Added strava api request test
    This is just an initial test to see how to connect to the Strava API,
    I used this article as an initial guide - https://javascript.plainenglish.io/strava-api-react-app-326e63527e2c
    
    Refactoring of this will follow with more better information provided so stay tuned.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [activities, setActivities] = useState({})
+
+  // TODO: Move the API request responsibility to Express and then React can just handle the display
+  // of data from the backend endpoint.
+  // Strava Credentials
+  let clientID = process.env.REACT_APP_CLIENT_ID;
+  let clientSecret = process.env.REACT_APP_CLIENT_SECRET;
+
+  // refresh token and call address
+  // TODO: Will need to extract the refreshToken from the url with an athlete
+  // authorises Strava to play nice with the app.
+  const refreshToken = process.env.REACT_APP_REFRESH_TOKEN;
+  const callRefresh = `https://www.strava.com/oauth/token?client_id=${clientID}&client_secret=${clientSecret}&refresh_token=${refreshToken}&grant_type=refresh_token`
+
+  // endpoint for read-all activities. temporary token is added in getActivities()
+  const callActivities = `https://www.strava.com/api/v3/athlete/activities?access_token=`
+
+  // Use refresh token to get current access token
+  useEffect(() => {
+    const fetchAccessToken = async () => {
+      try {
+        const fetchData = await fetch(callRefresh, {
+          method: 'POST'
+        })
+        const fetchJson = await fetchData.json()
+        getActivities(fetchJson.access_token)
+      } catch(e) {
+        console.log(e)
+      }
+    }
+    fetchAccessToken()
+  }, [callRefresh])
+
+  // use current access token to call all activities
+  async function getActivities(access) {
+      try {
+        const response = await fetch(callActivities + access);
+        const data = await response.json();
+        setActivities(data)
+        setIsLoading(false)
+      } catch(e){
+        console.log(e)
+      }
+  }
+
+  function showActivities() {
+    if (isLoading) return <>LOADING</>
+
+    return (
+      <>
+        <p>Last Strava activities: {activities.length}</p>
+        <ul>
+          {activities.map((activity, index) => (
+            <li key={index}>{activity.name}</li>
+          ))}
+        </ul>
+      </>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showActivities()}
     </div>
   );
 }
